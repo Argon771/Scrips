bd1c30c7b21f8c854ed4984c0feb2f2180d99849
Argon771 <105606683+Argon771@users.noreply.github.com>
2024-11-09 14:20:49 +0500

    Add files via upload

---
+++ 1.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+
+public class Crickets : MonoBehaviour
+
+{
+
+    // This script will print "crickets" to the console every frame
+
+    void Update()
+
+    {
+
+        Debug.Log("crickets");
+
+    }
+
+}
